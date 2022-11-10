7da4511dbbef6e25a6c7649279f9b2cd47f7ca5a
luminis7 <luminis@inbox.ru>
2022-11-10 16:31:34 +0300

    подчищены некрасивости

--- 04_cSharp_HW/Program.cs
+++ 04_cSharp_HW/Program.cs
@@ -60,7 +60,7 @@ int[] CreateRandomArray(int size)
 
     for(int i = 0; i < size; i++)
     {
-        Console.Write("Введите число: ");
+        Console.Write($"Введите {i}-й элемент массива: ");
         array[i] = Convert.ToInt32(Console.ReadLine());
     }
     return array;
@@ -71,7 +71,7 @@ void ShowArray(int[] array)
     for(int i = 0; i < array.Length; i++)
         Console.Write(array[i] + " ");
 
-    Console.WriteLine(" /n ");
+    Console.WriteLine(" \n");
 }
 
 Console.WriteLine("Введите количество элементов: ");
