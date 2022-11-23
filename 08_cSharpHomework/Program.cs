cc3c60a3a5bbae0a5f5bb518b3f5658c4b5c4873
luminis7 <luminis@inbox.ru>
2022-11-23 17:51:02 +0300

    add exercise 54, 56

---
+++ 08_cSharpHomework/Program.cs
@@ -0,0 +1,124 @@
+﻿int[,] CreateRandom2dArray(int rows, int columns, int minValue, int maxValue)
+{
+    int[,] array = new int[rows, columns];
+
+    for(int i = 0; i < rows; i++)
+        for(int j = 0; j < columns; j++)
+            array[i,j] = new Random().Next(minValue, maxValue + 1);
+
+    return array;
+}
+
+void Show2dArray(int[,] array)
+{
+    for(int i = 0; i < array.GetLength(0); i++)
+    {
+        for(int j = 0; j < array.GetLength(1); j++)
+            Console.Write(array[i,j] + " ");
+
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+Console.Write("Input a number of rows: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a number of columns: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a min possible value: ");
+int min = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a max possible value: ");
+int max = Convert.ToInt32(Console.ReadLine());
+
+int[,] myArray = CreateRandom2dArray(m, n, min, max);
+Show2dArray(myArray);
+
+// Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы 
+// каждой строки двумерного массива.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// В итоге получается вот такой массив:
+// 7 4 2 1
+// 9 5 3 2
+// 8 4 4 2
+/*
+void SortArray2dInDecreasing(int[,] array)
+{
+    for(int i = 0; i < array.GetLength(0); i++)
+    {
+        for(int j = 0; j < array.GetLength(1); j++)
+        {
+            for(int k = j + 1; k < array.GetLength(1); k++)
+            {
+                if(array[i,j] < array[i,k])
+                {
+                    int temp = array[i,j];
+                    array[i,j] = array[i,k];
+                    array[i,k] = temp;
+                }
+            }
+        }
+    }
+}
+SortArray2dInDecreasing(myArray);
+Show2dArray(myArray);
+*/
+
+// Задача 56: Задайте прямоугольный двумерный массив. Напишите программу, 
+// которая будет находить строку с наименьшей суммой элементов.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 5 2 6 7
+// Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов:
+// 1 строка
+
+int MinSumNumInRows(int[,] array)
+{
+    int minSumInRow = 0;
+    int indexMinSum = 0;
+    
+    for(int j = 0; j < array.GetLength(1);j++)
+        minSumInRow += array[0,j];
+    
+    for(int i = 1; i < array.GetLength(0); i++)
+    {
+        int sumNumInRow = 0;
+        for(int j = 0; j < array.GetLength(1); j++)
+        {
+            sumNumInRow += array[i,j];
+        }
+        if(sumNumInRow<minSumInRow)
+        {
+            minSumInRow = sumNumInRow;
+            indexMinSum = i;
+        }
+    }
+    
+    return indexMinSum;
+}
+Console.WriteLine($"минимальная сумма в строке номер {MinSumNumInRows(myArray)}");
+// Задача 58: Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
+// Например, даны 2 матрицы:
+// 2 4 | 3 4
+// 3 2 | 3 3
+// Результирующая матрица будет:
+// 18 20
+// 15 18
+
+// Задача 60. ...Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
+// Массив размером 2 x 2 x 2
+// 66(0,0,0) 25(0,1,0)
+// 34(1,0,0) 41(1,1,0)
+// 27(0,0,1) 90(0,1,1)
+// 26(1,0,1) 55(1,1,1)
+
+// Задача 62. Напишите программу, которая заполнит спирально массив 4 на 4.
+// Например, на выходе получается вот такой массив:
+// 01 02 03 04
+// 12 13 14 05
+// 11 16 15 06
+// 10 09 08 07
