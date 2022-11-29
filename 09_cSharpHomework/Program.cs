86499d4d52f02938f104574e59baf99b4b028254
luminis7 <luminis@inbox.ru>
2022-11-29 16:05:06 +0300

    add 09 homework

---
+++ 09_cSharpHomework/Program.cs
@@ -0,0 +1,56 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. 
+//Выполнить с помощью рекурсии.
+
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+/*
+void ShowNumsInDecris(int num)
+{
+    
+    if (num >= 1) 
+    {
+        Console.Write( num + " " );
+        ShowNumsInDecris(num - 1);
+    }
+}
+ShowNumsInDecris(7);
+*/
+
+// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов
+// в промежутке от M до N.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+/*
+int SunNum(int m, int n)
+{
+    if(m > n) return m + SunNum(m - 1, n);
+    if(m < n) return m + SunNum(m + 1, n);
+    return n;
+}
+
+Console.Write("Input a number M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a number N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"Sum of number between M and N is {SunNum(m, n)}");
+*/
+
+
+// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. 
+// Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+
+/*
+int AccermanFunction(int m, int n)
+{
+    if(m == 0) return (n+1);
+    if( m > 0 && n == 0) return AccermanFunction(m - 1, 1);
+    else return AccermanFunction(m - 1, AccermanFunction(m, n-1));
+}
+
+Console.WriteLine(AccermanFunction(3, 2));
+*/
